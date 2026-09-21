113607918f34a3be01cbc6543554d66c30b4025f
MichalCala <michal.cala22@gmail.com>
2026-09-21 17:02:39 +0200

    Render graph vertices, edges and labels

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -1,28 +1,64 @@
 
 #include <SFML/Graphics.hpp>
+
+#include <array>
 #include <optional>
+#include <string>
+#include <iostream>
+
+#include "Graph.hpp"
 
 int main()
 {
-    // Tworzymy okno o rozmiarze 900x600 pikseli
+    // 1. Tworzymy graf nieskierowany
+    Graph graph(10, false);
+
+    // Krawedzie z Twojego zadania
+    graph.AddEdge(0, 1);
+    graph.AddEdge(1, 2);
+    graph.AddEdge(2, 3);
+    graph.AddEdge(3, 4);
+    graph.AddEdge(3, 7);
+    graph.AddEdge(4, 5);
+    graph.AddEdge(5, 9);
+    graph.AddEdge(9, 9);
+    graph.AddEdge(6, 8);
+    graph.AddEdge(8, 6);
+    graph.AddEdge(0, 8);
+
+    // 2. Pozycje wierzcholkow
+    std::array<sf::Vector2f, 10> positions = {{
+        {110.f, 260.f},  // 0
+        {245.f, 160.f},  // 1
+        {370.f, 160.f},  // 2
+        {495.f, 160.f},  // 3
+        {610.f, 245.f},  // 4
+        {720.f, 350.f},  // 5
+        {340.f, 490.f},  // 6
+        {530.f, 55.f},   // 7
+        {220.f, 410.f},  // 8
+        {790.f, 460.f}   // 9
+    }};
+
+    // 3. Stworzenie okna
     sf::RenderWindow window(
         sf::VideoMode({900, 600}),
         "Graph Visualizer"
     );
 
     window.setFramerateLimit(60);
+    // Czcionka do wyswietlania numerow wierzcholkow
+    sf::Font font;
 
-    // Tworzymy pierwszy wierzcholek grafu
-    sf::CircleShape vertex(40.f);
+    if (!font.openFromFile("C:/Windows/Fonts/arial.ttf"))
+    {
+        std::cerr << "Nie udalo sie zaladowac czcionki!\n";
+        return 1;
+    }
 
-    vertex.setFillColor(sf::Color::Green);
-    vertex.setOrigin({40.f, 40.f});
-    vertex.setPosition({450.f, 300.f});
-
-    // Glowna petla programu
     while (window.isOpen())
     {
-        // Obsluga zdarzen, np. zamkniecia okna
+        // Obsluga zamykania okna
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -31,13 +67,95 @@ int main()
             }
         }
 
-        // Czyscimy ekran
         window.clear(sf::Color(30, 30, 30));
 
-        // Rysujemy wierzcholek
+        // 5. Rysowanie krawedzi
+        sf::VertexArray edges(sf::PrimitiveType::Lines);
+
+        for (int from = 0;
+             from < graph.NumberOfVertices();
+             ++from)
+        {
+            for (int to = from;
+                 to < graph.NumberOfVertices();
+                 ++to)
+            {
+                if (!graph.IsEdge(from, to))
+                {
+                    continue;
+                }
+
+                // Petla wlasna, np. 9 -> 9
+                if (from == to)
+                {
+                    sf::CircleShape loop(14.f);
+
+                    loop.setFillColor(sf::Color::Transparent);
+                    loop.setOutlineColor(sf::Color(170, 170, 170));
+                    loop.setOutlineThickness(2.f);
+
+                    loop.setPosition(
+                        positions[from] +
+                        sf::Vector2f{10.f, -42.f}
+                    );
+
+                    window.draw(loop);
+
+                    continue;
+                }
+
+                // Zwykla krawedz
+                edges.append(
+                    sf::Vertex{
+                        positions[from],
+                        sf::Color(170, 170, 170)
+                    }
+                );   
+
+                edges.append(
+                    sf::Vertex{
+                        positions[to],
+                        sf::Color(170, 170, 170)
+                    }
+                );
+            }
+        }
+
+        window.draw(edges);
+
+ 
+    // 6. Rysowanie wierzcholkow i ich numerow
+    for (int i = 0; i < graph.NumberOfVertices(); ++i)
+    {
+        // Rysujemy niebieskie kolo
+        sf::CircleShape vertex(22.f);
+
+        vertex.setOrigin({22.f, 22.f});
+        vertex.setPosition(positions[i]);
+        vertex.setFillColor(sf::Color(70, 150, 255));
+
         window.draw(vertex);
 
-        // Wyswietlamy narysowana klatke
+        // Tworzymy napis z numerem wierzcholka
+        sf::Text text(font, std::to_string(i), 20);
+
+        text.setFillColor(sf::Color::White);
+
+        // Wyznaczamy srodek napisu
+        sf::FloatRect bounds = text.getLocalBounds();
+
+        text.setOrigin({
+            bounds.position.x + bounds.size.x / 2.f,
+            bounds.position.y + bounds.size.y / 2.f
+        });
+
+        // Umieszczamy napis na srodku kola
+        text.setPosition(positions[i]);
+
+        window.draw(text);
+    }
+
+        // 7. Wyswietlenie gotowej klatki
         window.display();
     }
 
